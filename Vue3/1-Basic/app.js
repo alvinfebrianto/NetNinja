a40adef904da37cffe2484934c61147e294f6488
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-04-30 11:10:48 +0700

    Conditional Rendering - Vue Basic

--- Vue3/1-Basic/app.js
+++ Vue3/1-Basic/app.js
@@ -1,11 +1,19 @@
 const app = Vue.createApp({
   // template: `<h2>I am the template</h2>`
   data() {
-    return { title: "One Piece", author: "Eiichiro Oda", age: 49 };
+    return {
+      showBooks: true,
+      title: "One Piece",
+      author: "Eiichiro Oda",
+      age: 49,
+    };
   },
   methods: {
     changeTitle() {
       this.title = "Spy x Family";
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
   },
 });
