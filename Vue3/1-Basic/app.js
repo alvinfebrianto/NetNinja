1e9ee780cc876cce60d5853f2e621af4d8f5c950
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-04-30 10:48:29 +0700

    Click Events - Vue Basic

--- Vue3/1-Basic/app.js
+++ Vue3/1-Basic/app.js
@@ -3,6 +3,11 @@ const app = Vue.createApp({
   data() {
     return { title: "One Piece", author: "Eiichiro Oda", age: 49 };
   },
+  methods: {
+    changeTitle() {
+      this.title = "Spy x Family";
+    },
+  },
 });
 
 app.mount("#app");
