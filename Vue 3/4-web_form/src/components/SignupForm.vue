b247d3f8ea5d1b7e013f4db8a4d2e2d77af69a33
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-06-19 23:17:26 +0700

    Select Fields

--- Vue 3/4-web_form/src/components/SignupForm.vue
+++ Vue 3/4-web_form/src/components/SignupForm.vue
@@ -4,15 +4,21 @@
     <input type="email" required v-model="email" />
     <label>Password:</label>
     <input type="password" required v-model="password" />
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
 </template>
 
 <script>
 export default {
   data() {
-    return { email: "", password: "" };
+    return { email: "", password: "", role: "" };
   },
 };
 </script>
@@ -35,7 +41,8 @@ label {
   letter-spacing: 1px;
   font-weight: bold;
 }
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
