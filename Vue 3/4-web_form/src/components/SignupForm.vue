fa16287b34c5717c101bc0eac407b1052d4f18c4
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-06-29 13:44:03 +0700

    Keyboard Events & Modifiers

--- Vue 3/4-web_form/src/components/SignupForm.vue
+++ Vue 3/4-web_form/src/components/SignupForm.vue
@@ -2,37 +2,53 @@
   <form>
     <label>Email:</label>
     <input type="email" required v-model="email" />
+
     <label>Password:</label>
     <input type="password" required v-model="password" />
+
     <label>Role:</label>
     <select v-model="role">
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">{{ skill }}</div>
+
     <div class="terms">
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
     </div>
-    <div>
-      <input type="checkbox" value="alvin" v-model="names" />
-      <label>Alvin</label>
-    </div>
-    <div>
-      <input type="checkbox" value="febrianto" v-model="names" />
-      <label>Febrianto</label>
-    </div>
   </form>
+
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
   <p>Terms accepted: {{ terms }}</p>
-  <p>Names: {{ names }}</p>
 </template>
 
 <script>
 export default {
   data() {
-    return { email: "", password: "", role: "", terms: false, names: [] };
+    return {
+      email: "",
+      password: "",
+      role: "",
+      terms: false,
+      tempSkill: "",
+      skills: [],
+    };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = "";
+      }
+    },
   },
 };
 </script>
