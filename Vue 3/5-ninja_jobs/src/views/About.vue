1f42883e61308e06860100f732415e69405d2e47
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-06-30 21:00:32 +0700

    Router Links

--- Vue 3/5-ninja_jobs/src/views/About.vue
+++ Vue 3/5-ninja_jobs/src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cum
+      eligendi distinctio exercitationem ut voluptate inventore itaque, repellat
+      temporibus consectetur.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cum
+      eligendi distinctio exercitationem ut voluptate inventore itaque, repellat
+      temporibus consectetur.
+    </p>
   </div>
 </template>
--- Vue 3/5-ninja_jobs/src/views/Home.vue
+++ Vue 3/5-ninja_jobs/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cum
+      eligendi distinctio exercitationem ut voluptate inventore itaque, repellat
+      temporibus consectetur.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cum
+      eligendi distinctio exercitationem ut voluptate inventore itaque, repellat
+      temporibus consectetur.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
